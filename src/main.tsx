8a6a279ec6647ff17bafd8833f4a47a9ef25435c
dharshangscreations <g.s.dharshan384@gmail.com>
2026-04-29 19:15:43 +0530

    Reduce lenis scroll delay

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@
   import "./styles/index.css";
 
   createRoot(document.getElementById("root")!).render(
-    <ReactLenis root>
+    <ReactLenis root options={{ lerp: 0.2, wheelMultiplier: 1.2 }}>
       <App />
     </ReactLenis>
   );
