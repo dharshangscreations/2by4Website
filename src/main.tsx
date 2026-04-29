89f9c6c8415062346d92d2231a3c881745c47065
dharshangscreations <g.s.dharshan384@gmail.com>
2026-04-29 19:14:28 +0530

    Add lenis for smooth scrolling

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 
   import { createRoot } from "react-dom/client";
+  import { ReactLenis } from "@studio-freight/react-lenis";
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  createRoot(document.getElementById("root")!).render(
+    <ReactLenis root>
+      <App />
+    </ReactLenis>
+  );
   
